7a7e5c8b51e2a60c63cd5ee66b96a718c9c6465d
Sabby <sabby_india@hotmail.com>
2019-01-23 14:18:59 -0600

    Node HTTP Example 2 with error

--- index_jan22.js
+++ index_jan22.js
@@ -8,6 +8,13 @@ const port = 3000;
 const server = http.createServer((req,res) => {
 
     console.log('Request for '+ req.url + ' by method '+ req.method);
+
+   
+        /*
+            res.statusCode = 200;
+            res.setHeader('Content-Type','text/html');
+            fs.createReadStream('./public/index.html').pipe(res);
+            */
     
     if(req.method == 'GET'){
         var fileUrl;
@@ -17,7 +24,7 @@ const server = http.createServer((req,res) => {
             fileUrl= req.url;
         
             var filepath = path.resolve('./public'+fileUrl);
-            const fileExt = filepath.extname(filepath);
+           /* const fileExt = filepath.extname(filepath);
             if(fileExt == '.html'){
                 fs.exists(filepath,(exists) =>{
                     if(!exists){
@@ -38,8 +45,11 @@ const server = http.createServer((req,res) => {
                 res.setHeader('Content-Type','text/html');
                 res.end('<html><body><h1>Error 404:'+ fileUrl+ 'is not html file</h1></body></html>');
                 
-            }           
-           
+                
+            }*/
+            res.statusCode = 200;
+            res.setHeader('Content-Type','text/html');
+            fs.createReadStream(filepath).pipe(res);
     }
     else{
 
